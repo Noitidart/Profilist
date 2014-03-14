1c27fe0b47fc1b7f74d94cc5d5ae993c73e70fb4
Noitidart <noitidart@gmail.com>
2014-03-14 03:48:33 -0700

    initial
    bootstrap.js
    -DOMWin enum boilerplate
    
    install.rdf
    -set icon contrib
    -minVer for australis

---
+++ bootstrap.js
@@ -0,0 +1,68 @@
+const {interfaces: Ci, utils: Cu} = Components;
+const self = {
+	name: 'Profilist',
+	aData: 0,
+};
+
+Cu.import('resource://gre/modules/Services.jsm');
+
+/*start - windowlistener*/
+var windowListener = {
+	//DO NOT EDIT HERE
+	onOpenWindow: function (aXULWindow) {
+		// Wait for the window to finish loading
+		let aDOMWindow = aXULWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+		aDOMWindow.addEventListener("load", function () {
+			aDOMWindow.removeEventListener("load", arguments.callee, false);
+			windowListener.loadIntoWindow(aDOMWindow);
+		}, false);
+	},
+	onCloseWindow: function (aXULWindow) {},
+	onWindowTitleChange: function (aXULWindow, aNewTitle) {},
+	register: function () {
+		// Load into any existing windows
+		let DOMWindows = Services.wm.getEnumerator(null);
+		while (DOMWindows.hasMoreElements()) {
+			let aDOMWindow = DOMWindows.getNext();
+			windowListener.loadIntoWindow(aDOMWindow);
+		}
+		// Listen to new windows
+		Services.wm.addListener(windowListener);
+	},
+	unregister: function () {
+		// Unload from any existing windows
+		let DOMWindows = Services.wm.getEnumerator(null);
+		while (DOMWindows.hasMoreElements()) {
+			let aDOMWindow = DOMWindows.getNext();
+			windowListener.unloadFromWindow(aDOMWindow);
+		}
+		//Stop listening so future added windows dont get this attached
+		Services.wm.removeListener(windowListener);
+	},
+	//END - DO NOT EDIT HERE
+	loadIntoWindow: function (aDOMWindow) {
+		if (!aDOMWindow) {
+			return;
+		}
+		
+	},
+	unloadFromWindow: function (aDOMWindow) {
+		if (!aDOMWindow) {
+			return;
+		}
+	}
+};
+/*end - windowlistener*/
+function startup(aData, aReason) {
+	//self.aData = aData; //must go first, because functions in loadIntoWindow use self.aData
+	windowListener.register();
+}
+
+function shutdown(aData, aReason) {
+	if (aReason == APP_SHUTDOWN) return;
+	windowListener.unregister();
+}
+
+function install() {}
+
+function uninstall() {}
