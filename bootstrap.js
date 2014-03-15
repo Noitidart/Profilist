cb3eb443877e47e574a868df7ef114e83128568a
Noitidart <noitidart@gmail.com>
2014-03-15 14:13:31 -0700

    initial.rev1
    still in dev mode
    learned: to put stack element in panel, it doesnt behave as stack it
    behaves as display:block, this is because its parent <panelview> is set
    to display:flex, so i have to set create a box, then inside the box add
    the stack, now the children of stack properly stack on each other. even
    if put in style tag of stack, if its not in a box, to be
    display:-moz-box; and even if set it to important, it won't act as box
    until put it in a non-flex element
    my css on the PanelUI-profilist is not right for some reason so i had to
    force it with style attribute
    width on stack is not matching full width unless i set style width 100%

--- bootstrap.js
+++ bootstrap.js
@@ -1,10 +1,22 @@
-const {interfaces: Ci, utils: Cu} = Components;
+const {interfaces: Ci, utils: Cu, classes: Cc} = Components;
 const self = {
 	name: 'Profilist',
+	chrome_path: 'chrome://profilist/content/',
 	aData: 0,
 };
 
+const myServices = {};
+var cssUri;
+
 Cu.import('resource://gre/modules/Services.jsm');
+Cu.import('resource://gre/modules/devtools/Console.jsm');
+Cu.import('resource://gre/modules/XPCOMUtils.jsm');
+XPCOMUtils.defineLazyGetter(myServices, 'sss', function(){ return Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService) });
+
+function prevHide(e) {
+	e.preventDefault();
+	e.stopPropagation();
+}
 
 /*start - windowlistener*/
 var windowListener = {
@@ -45,21 +57,131 @@ var windowListener = {
 			return;
 		}
 		
+		var PanelUI = aDOMWindow.document.querySelector('#PanelUI-popup');
+		if (PanelUI) {
+			var PUIf = aDOMWindow.document.querySelector('#PanelUI-footer');
+			var PUIcs = aDOMWindow.document.querySelector('#PanelUI-contents-scroller');
+			console.log('PUIcs.style.width',PUIcs.style.width);
+			var profilistHBoxJSON =
+			['xul:vbox', {id: 'profilist_hbox'},
+				['xul:stack', {key:'profilist_stack',style:'width:100%'}]
+			];
+			var referenceNodes = {};
+			PUIf.insertBefore(jsonToDOM(profilistHBoxJSON, aDOMWindow.document, referenceNodes), PUIf.firstChild);
+			
+			var PUIsync = aDOMWindow.document.querySelector('#PanelUI-fxa-status')
+			var dupeNode1 = PUIsync.cloneNode(true);
+			dupeNode1.setAttribute('id', 'PanelUI-profilist');
+			dupeNode1.setAttribute('style', '-moz-appearance:none; padding:10px 0 10px 15px; margin-bottom:-1px; border-top:1px solid rgba(24,25,26,0.14); border-bottom:1px solid transparent; border-right:1px solid transparent;');
+			
+			var dupeNode2 = PUIsync.cloneNode(true);
+			
+			referenceNodes.profilist_stack.appendChild(dupeNode1);
+			referenceNodes.profilist_stack.appendChild(dupeNode2);
+			PanelUI.addEventListener('popuphiding', prevHide, false)
+		}
+		
 	},
 	unloadFromWindow: function (aDOMWindow) {
 		if (!aDOMWindow) {
 			return;
 		}
+		
+		var PanelUI = aDOMWindow.document.querySelector('#PanelUI-popup');
+		if (PanelUI) {
+			PanelUI.removeEventListener('popuphiding', prevHide, false)
+			var profilistHBox = aDOMWindow.document.querySelector('#profilist_hbox');
+			profilistHBox.parentNode.removeChild(profilistHBox);
+		}
 	}
 };
 /*end - windowlistener*/
+
+/*dom insertion library function from MDN - https://developer.mozilla.org/en-US/docs/XUL_School/DOM_Building_and_HTML_Insertion*/
+jsonToDOM.namespaces = {
+    html: 'http://www.w3.org/1999/xhtml',
+    xul: 'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul'
+};
+jsonToDOM.defaultNamespace = jsonToDOM.namespaces.html;
+function jsonToDOM(xml, doc, nodes) {
+    function namespace(name) {
+        var m = /^(?:(.*):)?(.*)$/.exec(name);        
+        return [jsonToDOM.namespaces[m[1]], m[2]];
+    }
+
+    function tag(name, attr) {
+        if (Array.isArray(name)) {
+            var frag = doc.createDocumentFragment();
+            Array.forEach(arguments, function (arg) {
+                if (!Array.isArray(arg[0]))
+                    frag.appendChild(tag.apply(null, arg));
+                else
+                    arg.forEach(function (arg) {
+                        frag.appendChild(tag.apply(null, arg));
+                    });
+            });
+            return frag;
+        }
+
+        var args = Array.slice(arguments, 2);
+        var vals = namespace(name);
+        var elem = doc.createElementNS(vals[0] || jsonToDOM.defaultNamespace, vals[1]);
+
+        for (var key in attr) {
+            var val = attr[key];
+            if (nodes && key == 'key')
+                nodes[val] = elem;
+
+            vals = namespace(key);
+            if (typeof val == 'function')
+                elem.addEventListener(key.replace(/^on/, ''), val, false);
+            else
+                elem.setAttributeNS(vals[0] || '', vals[1], val);
+        }
+        args.forEach(function(e) {
+			try {
+				elem.appendChild(
+									Object.prototype.toString.call(e) == '[object Array]'
+									?
+										tag.apply(null, e)
+									:
+										e instanceof doc.defaultView.Node
+										?
+											e
+										:
+											doc.createTextNode(e)
+								);
+			} catch (ex) {
+				elem.appendChild(doc.createTextNode(ex));
+			}
+        });
+        return elem;
+    }
+    return tag.apply(null, xml);
+}
+/*end - dom insertion library function from MDN*/
+
 function startup(aData, aReason) {
-	//self.aData = aData; //must go first, because functions in loadIntoWindow use self.aData
+	self.aData = aData; //must go first, because functions in loadIntoWindow use self.aData
+	console.log('aData', aData);
+	//var css = '.findbar-container {-moz-binding:url(' + self.path.chrome + 'findbar.xml#matchword_xbl)}';
+	//var cssEnc = encodeURIComponent(css);
+	var newURIParam = {
+		aURL: self.aData.resourceURI.spec + 'main.css', //'data:text/css,' + cssEnc,
+		aOriginCharset: null,
+		aBaseURI: null
+	}
+	cssUri = Services.io.newURI(newURIParam.aURL, newURIParam.aOriginCharset, newURIParam.aBaseURI);
+	myServices.sss.loadAndRegisterSheet(cssUri, myServices.sss.USER_SHEET);
+	
 	windowListener.register();
 }
 
 function shutdown(aData, aReason) {
 	if (aReason == APP_SHUTDOWN) return;
+	
+	myServices.sss.unregisterSheet(cssUri, myServices.sss.USER_SHEET);
+	
 	windowListener.unregister();
 }
 
