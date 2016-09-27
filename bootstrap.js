382ead7db368cace75e04dd19ddb9ff8c1434e65
Noitidart <noitidart@gmail.com>
2016-09-27 13:09:13 -0700

    5.0b.rev1 * Wrote up install and uninstall procs for native messaging * Brought it exe submodule, doesnt have anything it

--- bootstrap.js
+++ bootstrap.js
@@ -8,6 +8,7 @@ Cu.import('resource://gre/modules/Services.jsm');
 // Globals
 var core = {
 	addon: {
+		id: 'Profilist@jetpack',
 		version: null, // populated by `startup`
 		path: {
 			name: 'profilist',
@@ -30,12 +31,12 @@ var core = {
 			// as now using webext, i have to send it these paths
 		}
 	}
-	// os: {
-	// 	// // name: OS.Constants.Sys.Name, // added by worker
-	// 	// // mname: added by worker
-	// 	toolkit: Services.appinfo.widgetToolkit.toLowerCase(),
-	// 	xpcomabi: Services.appinfo.XPCOMABI
-	// },
+	os: {
+		// // name: OS.Constants.Sys.Name, // added by worker
+		// // mname: added by worker
+		// toolkit: Services.appinfo.widgetToolkit.toLowerCase(),
+		// xpcomabi: Services.appinfo.XPCOMABI
+	},
 	firefox: {
 		pid: Services.appinfo.processID,
 		version: Services.appinfo.version,
@@ -45,10 +46,30 @@ var core = {
 
 var gAndroidMenuIds = [];
 
+// set addon paths
+core.addon.path.storage = OS.Path.join(OS.Constants.Path.profileDir, 'jetpack', core.addon.id, 'simple-storage');
+core.addon.path.filestore = OS.Path.join(core.addon.path.storage, 'store.json');
+
+// set the paths
+core.profilist.path.dirstore = OS.Path.join(OS.Constants.Path.userApplicationDataDir, 'profilist');
+core.profilist.path.exestore = OS.Path.join(core.profilist.path.dirstore, 'exe');
+core.profilist.path.iconstore = OS.Path.join(core.profilist.path.dirstore, 'icon');
+core.profilist.path.iconimgsstore = OS.Path.join(core.profilist.path.dirstore, 'iconset'); // images that are used to make file in `iconstore`
+core.profilist.path.inibkp = OS.Path.join(core.profilist.path.dirstore, 'profiles.profilist.ini');
+
+// add the OS.Constants.Path's because webext doesnt have it
+core.addon.path.homeDir = OS.Constants.Path.homeDir;
+core.addon.path.userApplicationDataDir = OS.Constants.Path.userApplicationDataDir;
+core.addon.path.profileDir = OS.Constants.Path.profileDir;
+core.addon.path.localProfileDir = OS.Constants.Path.localProfileDir;
+
 function install() {}
 function uninstall(aData, aReason) {
     if (aReason == ADDON_UNINSTALL) {
-		// OS.File.removeDir(OS.Path.join(OS.Constants.Path.profileDir, 'jetpack', core.addon.id), {ignorePermissions:true, ignoreAbsent:true}); // will reject if `jetpack` folder does not exist
+		// delete storage
+		OS.File.removeDir(core.addon.path.storage, {ignorePermissions:true, ignoreAbsent:true}); // will reject if `jetpack` folder does not exist
+
+		uninstallNativeMessaging();
 	}
 }
 
@@ -66,50 +87,9 @@ function startup(aData, aReason) {
 	// set version
 	core.addon.version = aData.version;
 
-	// set the paths
-	core.profilist.path.dirstore = OS.Path.join(OS.Constants.Path.userApplicationDataDir, 'profilist');
-	core.profilist.path.exestore = OS.Path.join(core.profilist.path.dirstore, 'exe');
-	core.profilist.path.iconstore = OS.Path.join(core.profilist.path.dirstore, 'icon');
-	core.profilist.path.iconimgsstore = OS.Path.join(core.profilist.path.dirstore, 'iconset'); // images that are used to make file in `iconstore`
-	core.profilist.path.inibkp = OS.Path.join(core.profilist.path.dirstore, 'profiles.profilist.ini');
+	promiseallarr.push( installNativeMessaging() );
 
-	// add the OS.Constants.Path's because webext doesnt have it
-	core.profilist.path.userApplicationDataDir = OS.Constants.Path.userApplicationDataDir;
-	core.profilist.path.profileDir = OS.Constants.Path.profileDir;
-	core.profilist.path.localProfileDir = OS.Constants.Path.localProfileDir;
-
-	// copy the exes
-	var exename = 'profilist';
-	var sname = OS.Constants.Sys.Name.toLowerCase(); // stands for "simplified name". different from `mname`
-	if (sname.startsWith('win')) {
-		sname = 'win';
-		exename += '.exe';
-	} else if (sname == 'darwin') {
-		sname = 'mac';
-	} else {
-		sname = 'nix';
-	}
-	var exepath = OS.Path.join(core.profilist.path.dirstore, exename);
-	promiseallarr.push(new Promise( (resolve, reject) => {
-		OS.File.copy(OS.Path.join(core.addon.path.exe, sname, exename), OS.Path.join(core.profilist.path.dirstore, exename), { noOverwrite:true }).then( copy_ok => resolve(true) ).catch( OSFileError => OSFileError.becauseExists() ? resolve(false) : reject(OSFileError) );
-	}));
-
-	// make sure exe manifest is in place
-	var exemanifest_path;
-	switch (sname) {
-
-	}
-	promiseallarr.push(new Promise( resolve => {
-		if (OS.File.exists()) {
-
-		}
-	}));
-
-	// if windows make sure registry is updated
-	promiseallarr.push(new Promise( resolve => {
-		if (OS.File.exists())
-	}));
-
+	// wait for all promises, then startup webext
 	Promise.all(promiseallarr).then(() => {
 		aData.webExtension.startup().then(api => {
 			({ browser:webext } = api);
@@ -139,6 +119,125 @@ function shutdown(aData, aReason) {
 
 	windowListener.unregister();
 
+}
+
+// start - native messaging stuff
+function getNativeMessagingInfo() {
+	// returns { os_sname, exe_path, exe_from, exe_name, exemanifest_path, exemanifest_from, exemanifest_json, winregistry_path }
+	// `winregistry_path` is undefined if not windows
+	// os_sname is win/mac/nix
+
+	var exemanifest_json = {
+	  'name': 'profilist', // i use this as child entry for windows registry entry, so make sure this name is compatible with injection into windows registry link39191
+	  'description': 'Platform helper for Profilist',
+	  'path': undefined, // set below to `exe_path`,
+	  'type': 'stdio',
+	  'allowed_extensions': [ core.addon.id ]
+	};
+
+	var exe_name;
+	var sname = OS.Constants.Sys.Name.toLowerCase(); // stands for "simplified name". different from `mname`
+	if (sname.startsWith('win')) {
+		sname = 'win';
+		exe_name = exemanifest_json.name + '.exe';
+	} else if (sname == 'darwin') {
+		sname = 'mac';
+		exe_name = exemanifest_json.name;
+	} else {
+		sname = 'nix';
+		exe_name = exemanifest_json.name;
+	}
+
+	var exe_path;
+	exe_path = OS.Path.join(core.addon.path.userApplicationDataDir, 'extension-exes', exe_name);
+
+	var exe_from = core.addon.path.userApplicationDataDir; // dir that exists for sure in subpath of `exe_path`. where to `makeDir` from for exe
+
+	// update exemanifest_json
+	exemanifest_json.path = exe_path;
+	exemanifest_json.description += ' for ' + sname;
+
+	var exemanifest_path;
+	var exemanifest_from; // dir that exists for sure in subpath of `exemanifest_path`. where to `makeDir` from for exe
+	switch (sname) {
+		case 'win':
+				exemanifest_path = OS.Path.join(core.addon.path.filestore, 'profilist.json');
+				exemanifest_from = core.profilist.path.profileDir;
+			break;
+		case 'mac':
+				exemanifest_path = OS.Path.join(core.profilist.path.homeDir, 'Library', 'Application Support', 'Mozilla', 'NativeMessagingHosts', 'profilist.json');
+				exemanifest_from = OS.Path.join(core.profilist.path.homeDir, 'Library', 'Application Support');
+			break;
+		case 'nix':
+				exemanifest_path = OS.Path.join(core.profilist.path.homeDir, '.mozilla', 'native-messaging-hosts', 'profilist.json');
+				exemanifest_from = core.profilist.path.homeDir;
+			break;
+	}
+
+	var winregistry_path;
+	if (sname == 'win') {
+		winregistry_path = 'SOFTWARE\\Mozilla\\NativeMessagingHosts';
+	}
+
+	return { os_sname:sname, exe_path, exe_from, exe_name, exemanifest_path, exemanifest_from, exemanifest_json, winregistry_path };
+
+}
+
+function installNativeMessaging() {
+	var { exe_path, exe_from, exe_name, exemanifest_path, exemanifest_from, exemanifest_json, os_sname } = getNativeMessagingInfo();
+
+	var promiseallarrmain = [];
+
+	// copy the exes
+	promiseallarrmain.push(new Promise(resolve, reject) =>
+		OS.File.makeDir(OS.Path.dirname(exe_path), { from:exe_from }).then( dirmade => {
+			OS.File.copy(core.addon.path.exe + '/' + os_sname + '/' exe_name), exe_path).then( copied => resolve() ).catch( osfileerr => reject(osfileerr) );
+		} ).catch(osfileerr => reject(osfileerr));
+	);
+
+	// make sure exe manifest is in place
+	promiseallarrmain.push(new Promise( (resolve, reject) =>
+		writeThenDir(exemanifest_path, JSON.stringify(exemanifest_json), { from:exemanifest_from, noOverwrite:true, encoding:'utf-8' }).then( ok => resolve() ).catch( osfileerr => osfileerr.becauseExists ? resolve() : reject(osfileerr) )
+	));
+
+	// if Windows then update registry
+	if (winregistry_path) {
+		var wrk = Cc['@mozilla.org/windows-registry-key;1'].createInstance(Ci.nsIWindowsRegKey);
+		wrk.create(wrk.ROOT_KEY_CURRENT_USER,   + '\\' + exemanifest_json.name, wrk.ACCESS_WRITE); // link39191
+		wrk.writeStringValue('', exemanifest_path);
+		wrk.close();
+		// not ignoring errors during write, if it errors, startup fails
+	}
+
+	return Promise.all(promiseallarrmain);
+}
+
+function uninstallNativeMessaging() {
+	var { exe_path, exe_from, exe_name, exemanifest_path, exemanifest_from, exemanifest_json, winregistry_path } = getNativeMessagingInfo();
+
+	var promiseallarrmain = [];
+
+	// delete exe
+	promiseallarrmain.push( OS.File.remove(exe_path, { ignorePermissions:true, ignoreAbsent:true }) ); // ignoreAbsent because maybe another profile already deleted it
+
+	// TODO: if `exe_path` parent dir is empty, remove it, because parent dir is my own created one of "extensions-exes"
+
+	// delete manifest
+	promiseallarrmain.push( OS.File.remove(exemanifest_path, { ignorePermissions:true, ignoreAbsent:true }) ); // ignoreAbsent because if windows, then its in core.addon.path.filestore which is already deleted by now
+
+	// if Windows then update registry
+	if (winregistry_path) {
+		var wrk = Cc['@mozilla.org/windows-registry-key;1'].createInstance(Ci.nsIWindowsRegKey);
+		try {
+			wrk.open(wrk.ROOT_KEY_CURRENT_USER, winregistry_path, wrk.ACCESS_WRITE);
+			wrk.removeChild(exemanifest_json.name); // link39191
+		} finally {
+			wrk.close();
+		}
+		// NOTE: i am ignoring errors that happen during uninstall from registry
+	}
+
+	return Promise.all(promiseallarrmain);
 }
 
 // start - addon functions
@@ -255,4 +354,36 @@ function getNativeHandlePtrStr(aDOMWindow) {
 								   .QueryInterface(Ci.nsIInterfaceRequestor)
 								   .getInterface(Ci.nsIBaseWindow);
 	return aDOMBaseWindow.nativeHandle;
+}
+// rev2 - https://gist.github.com/Noitidart/5257376b54935556173e8d13c2821f4e
+function writeThenDirMT(aPlatPath, aContents, aDirFrom, options={}) {
+	// tries to writeAtomic
+	// if it fails due to dirs not existing, it creates the dir
+	// then writes again
+	// if fail again for whatever reason it rejects with `osfileerr`
+	// on success resolves with `true`
+
+	var default_options = {
+		encoding: 'utf-8',
+		noOverwrite: false
+		// tmpPath: aPlatPath + '.tmp'
+	};
+
+	options = Object.assign(default_options, options);
+
+	var writeTarget = () => OS.File.writeAtomic(aPlatPath, aContents, options);
+
+	return new Promise((resolvemain, rejectmain) =>
+		writeTarget().then( ()=>resolvemain() ).catch( osfileerr => {
+			if (osfileerr.becauseNoSuchFile) { // this happens when directories dont exist to it
+				OS.File.makeDir(OS.Path.dirname(aPlatPath), {from:aDirFrom}).then(
+					()=>writeTarget().then( ()=>resolvemain() ).catch( osfileerr=>rejectmain(osfileerr) )
+				// ).catch( osfileerr=>resolvemain(false) );
+				).catch( osfileerr=>rejectmain(osfileerr) );
+			} else {
+				rejectmain(osfileerr);
+				// resolvemain(false);
+			}
+		})
+	);
 }
