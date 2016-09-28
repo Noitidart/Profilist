651a1fdf720382772020b048b78d2e4096eaf365
Noitidart <noitidart@gmail.com>
2016-09-28 12:19:46 -0700

    5.0b.rev4
    * opening tab

--- webextension/scripts/background.js
+++ webextension/scripts/background.js
@@ -1,8 +1,16 @@
 console.error('in bg.js');
 var port = browser.runtime.connectNative('profilist');
-port.onMessage.addListener(response => console.log('Received: ', response) );
+port.onMessage.addListener(function (response) {
+	console.log('Received: ', response);
+});
 
-browser.browserAction.onClicked.addListener(() => {
+browser.browserAction.onClicked.addListener(function() {
+
 	console.log('Sending:  ping');
 	port.postMessage('ping');
+
+	chrome.tabs.create({
+		url: chrome.extension.getURL('my-page.html')
+	});
+
 });
