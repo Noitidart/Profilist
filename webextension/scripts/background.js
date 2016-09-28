ca2796ffe46e4cde594f2b24c9200c08fd3e28c2
Noitidart <noitidart@gmail.com>
2016-09-28 09:29:21 -0700

    5.0b.rev3 *Ping pong native messaging working

--- webextension/scripts/background.js
+++ webextension/scripts/background.js
@@ -1,1 +1,8 @@
 console.error('in bg.js');
+var port = browser.runtime.connectNative('profilist');
+port.onMessage.addListener(response => console.log('Received: ', response) );
+
+browser.browserAction.onClicked.addListener(() => {
+	console.log('Sending:  ping');
+	port.postMessage('ping');
+});
